a8c2cd9baf84853a2a012a0550d3f470d99aa4c7
Bill Keese <bill@dojotoolkit.org>
2005-12-23 04:02:10 +0000

    clean up the mail demo a little bit (split javascript and css into separate files, so I don't cause a bad example :-) )
    git-svn-id: http://svn.dojotoolkit.org/src/trunk/demos@2584 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ widget/Mail/mail.js
@@ -0,0 +1,24 @@
+// Mail demo javascript code
+
+// Display list of messages (upper right pane)
+function displayList(){
+    this.update = function(message) {
+        var clickedTreeNode =     
+            dojo.widget.getWidgetById(message);
+
+		var listPane = dojo.widget.getWidgetById("listPane");
+		var url = "Mail/"+clickedTreeNode.title.replace(" ","") + ".html";
+		
+		listPane.setUrl(url);
+    };
+}
+var displayer = new displayList();
+var nodeSelectionTopic = dojo.event.topic.getTopic("listSelected");
+nodeSelectionTopic.subscribe(displayer, "update");
+
+// Display a single message (in bottom right pane)
+function displayMessage(name){
+		var contentPane = dojo.widget.getWidgetById("contentPane");
+		var url = "Mail/"+name.replace(" ","") + ".html";
+		contentPane.setUrl(url);
+}
