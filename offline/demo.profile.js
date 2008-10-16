044f7d2eda350ef28bb9ae515ade983f2b5d6c02
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2008-10-16 16:35:33 +0000

    Initial move of the offline demos out to the demo repository.  fixes #7880
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@15449 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ offline/demo.profile.js
@@ -0,0 +1,17 @@
+dependencies = {
+	layers: [
+		{
+			name: "../demos/offline/src.js",
+			resourceName: "demos.offline.src",
+			dependencies: [
+				"demos.offline.src"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ],
+		[ "demos", "../demos" ]
+	]
+}
---
+++ offline/src.js
@@ -0,0 +1,5 @@
+dojo.provide("demos.offline.src");
+
+dojo.require("dojo.parser");
+dojo.require("dijit._editor.RichText");
+dojo.require("dojox.off.offline");
