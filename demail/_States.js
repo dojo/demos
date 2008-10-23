9bca3297ac4ab9f74651b67f90d2a1428793f999
Alex Russell <slightlyoff@chromium.org>
2008-10-23 03:27:44 +0000

    quick hacks to rework the mail demo...lots of work needed
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@15505 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ demail/_States.js
@@ -0,0 +1,11 @@
+//	summary:
+//		acme._States is a mixin designed to help widget authors easily manage
+//		applying styles based on widget conditions and provides event handler
+//		stubs to ease the life of widget users
+//	description:
+//		supported states:
+//			* enabled/disabled
+//			* selected/unselected
+//			* focused/blured
+dojo.declare("acme._States", null, {
+});
