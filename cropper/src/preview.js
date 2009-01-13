0d16812e81c05d5b15f2de1def51326fc79b5c0e
Peter E Higgins <dante@dojotoolkit.org>
2009-01-13 14:20:09 +0000

    backing out ben's fix, and using nic rizzo's -- ben was scaling the glass size, nic's scales the target image, which makes more sense in this application as you are able to specify the glass size and would probably prefer it stay a fixed size. thanks nic/ben! !strict
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@16320 560b804f-0ae3-0310-86f3-f6aa0a117693

--- cropper/src/preview.js
+++ cropper/src/preview.js
@@ -7,6 +7,7 @@ dojo.require("dojo.dnd.move");
 	// a simple "wrap" function. availaing in `plugd`
 	// warning: node must be in the DOM before being wrapped.
 	var wrap = function(node, withTag){
+		// summary: wrap some node with a tag type.
 		var n = d.create(withTag);
 		d.place(n, node, "before");
 		d.place(node, n, "first");
@@ -18,30 +19,33 @@ dojo.require("dojo.dnd.move");
 	d.declare("image.Preview", dijit._Widget, {
 		// summary: A Behavioral widget adding a preview pane to any `<img>`
 		
-		scale:2,
+		// the size of the draggable/moveable window
 		glassSize:150,
-
+		// the size of the preview window relative to the glassSize
+		scale:2,
+		
+		// unsupported atm:
 		withMouseMove:false,
 		withDrag:true,
-
+		
 		// moveInterval: Int
 		//		Adjust the time the preview redraws from dnd operations (ms)
 		moveInterval: 50,
-
+		
 		// hoverable: Boolean
 		//		Does this image react to hovering to show/hide the dragger
 		hoverable: false,
-
+		
 		postCreate: function(){
-
+			
 			var gs = this.glassSize, 
 				s = this.scale;
 			
 			// wrap the target in a div so we can control it with dnd.
 			var mb = dojo.marginBox(this.domNode);
 			this.currentSize = mb;
-			console.log(mb.w, mb.h);
-			
+
+			// wrap the domNode in antoher div so parentDnd works
 			this.container = wrap(this.domNode, "div");
 			dojo.marginBox(this.container, mb);
 			
@@ -66,7 +70,8 @@ dojo.require("dojo.dnd.move");
 			}, dojo.body());
 			this._positionPicker();
 
-			// wrap the full image
+			// wrap the full image, so we can position it easily relative
+			// to the outer node. embedded position:rel/absolute/rel/abs here
 			var n = wrap(d.create('img', {
 						style:{ position: abs },
 				 		src: this.altSrc || this.domNode.src 
@@ -75,11 +80,11 @@ dojo.require("dojo.dnd.move");
 			d.style(this.domNode, pos, abs);
 			
 			this.image = d.query('img', this.preview)
-				// when it is loaded, then do the dnd stuff.
+				// when it is loaded, use those numbers:
 				.onload(d.hitch(this, function(e){
 
 					var tc = this.coords, 
-						gss = this.glassSize * this.scale,
+						s = this.scale,
 						ts = this.targetSize = { 
 							w: e.target.width, 
 							h: e.target.height 
@@ -89,9 +94,9 @@ dojo.require("dojo.dnd.move");
 						x: ts.w / tc.w, y: ts.h / tc.h
 					};
 					
-					d.style(this.picker, {
-						height: gss / tr.y + "px",
-						width: gss / tr.x + "px"
+					d.style(this.image, {
+						height: tc.h * s + "px",
+						width: tc.w * s + "px"
 					});
 					
 				}))[0];
@@ -102,17 +107,15 @@ dojo.require("dojo.dnd.move");
 				// as container? ugh. 
 				{ area: "content", within: true }
 			);
-
+			
 			// setup dnd behavior
 			d.subscribe("/dnd/move/start", this, "_startDnd");
 			d.subscribe("/dnd/move/stop", this, "_stopDnd");
-				
-				
-			if(d.isIE){
-				// janky IE bug. onload doesn't fire until reset of .src
-				this.image.src = this.image.src;
-			}
 			
+			// janky IE src issue fix:
+			d.isIE && (this.image.src = this.image.src);
+			
+			// only because our layout is fluid:
 			this.connect(d.global, "onresize", "_positionPicker");
 		},
 		
@@ -125,33 +128,32 @@ dojo.require("dojo.dnd.move");
 				top: tc.y + "px"
 			});
 		},
-
+		
 		_startDnd: function(n){
 			// listen for dnd Start, determine if we care:
-			if(true || !this._interval && n && n.node == this.picker){
+			if(!this._interval && n && n.node == this.picker){
 				// listen to doc onmousemove 
 				this._interval = this.connect(d.doc, "onmousemove", "_whileMoving");
 			}
 		},
-
+		
 		_stopDnd: function(){
 			// dnd operations are done, remove the timer
-			if(this._interval){
-				this.disconnect(this._interval);
-			}
+			this._interval && this.disconnect(this._interval);
 		},
-
+		
 		_whileMoving: function(){
 			// while dnd in progress, adjust the backgroundPosition of the preview
 			if(!this.ratio){ return; }
 			
-			// FIXME: this math is wrong. Image is too zoomed?
-			var s = this.picker, 
-				xy = d.coords(s), 
+			var s = this.scale,
+				xy = d.coords(this.picker), 
 				tc = this.coords, 
-				x = (xy.l - tc.l) * this.ratio.x, 
-				y = (xy.t - tc.t) * this.ratio.y;
+				x = (xy.l - tc.l) * s, 
+				y = (xy.t - tc.t) * s;
 			
+			// position the image relative to the pickers position 
+			// in the container.
 			d.style(this.image, {
 				top: "-" + y + "px", 
 				left: "-" + x + "px"
@@ -160,6 +162,9 @@ dojo.require("dojo.dnd.move");
 		
 		destroy: function(){
 			// destroy our domNodes. this is a behavioral widget.
+			// we can .destroy(true) and leave the image, but I
+			// this we're destroying it with "container" destruction.
+			// FIXME:
 			dojo.forEach(["preview","picker","container","image"], function(n){
 				dojo.destroy(this[n]);
 				delete this[n];
@@ -168,7 +173,7 @@ dojo.require("dojo.dnd.move");
 		}
 		
 	});
-
+	
 	// setup the query "plugin"
 	d.extend(d.NodeList, {
 		preview: function(args){
