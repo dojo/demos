3b8746676a9a67bd280459af7e604c97a2b669d5
Peter E Higgins <dante@dojotoolkit.org>
2009-01-17 13:11:54 +0000

    more images and demo functionality. i think it\'s done, with some minor css cleanups. \!strict
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@16370 560b804f-0ae3-0310-86f3-f6aa0a117693

--- cropper/src.js
+++ cropper/src.js
@@ -3,22 +3,11 @@ dojo.provide("demos.cropper.src");
 dojo.require("demos.cropper.src.Preview");
 dojo.require("dojox.analytics.Urchin");
 
-// experimental thought:
-// dojo . require("dojox.image.Lightbox");
+// remove to disable json/paging
+// dojo . require("demos.cropper.src.nav"); 
 
 ;(function(d, $){
 	
-//	// if testing from pre-1.3, a provide dojo.create() implemenetation
-//	if(!d.create && d.version.minor < 3 && dojo.version.major == 1){
-//		d.create = function(t, a, r, p){
-//			var n = d.doc.createElement(t);
-//			if(a){ d.attr(n, a) }
-//			if(r){ d.place(n, r, p) }
-//			return n;
-//		};
-//		d.destroy = d._destroyElement;
-//	}
-		
 	d.addOnLoad(function(){
 		
 		// basic loading indicator code:
@@ -36,40 +25,33 @@ dojo.require("dojox.analytics.Urchin");
 		// or if no ref needed: $("#me").preview();
 		
 		// setup the clicking for the thumbnails
-		$("#footing").onclick(function(e){
+		$("#picker").onclick(function(e){
 			e.preventDefault();
 			
 			// it's the link or the img
 			var et = e.target,
 				src = et.parentNode.href || et.href;
-				
-			if(src){
+			
+			console.log(et, src);
+			
+			if(src && preview.image.src != src){
 				// show the loader after each click
 				show.play();
 				// when we have a src to load, set both images
 				preview.domNode.src = src;
 				preview.image.src = src; 
 				// update the title text:
-				d.byId("titleText").innerHTML = preview.image.alt = et.alt;
+			//	d.byId("titleText").innerHTML = preview.image.alt = et.alt;
 			}
 			
 		});
 		
-		// just don't load Lightbox resource if you don't want this:
-		if(dojox && dojox.image && dojox.image.Lightbox){
-			
-			var lb = new dojox.image.LightboxDialog(); lb.startup();
-			d.connect(preview.preview, "onclick", function(e){
-				// if we click on the preview node of our Preview widget, 
-				// show the image in a lightbox:
-				lb.show({ 
-					href: preview.image.src, 
-					title:preview.title 
-				});
-			});
-			
-		}
-		
+		// hook up the nav.js link in footer text:
+		var c = d.connect(dojo.byId("navjs"), "onclick", function(e){
+			d.disconnect(c); // only do this one time
+			dojo["require"]("demos.cropper.src.nav"); // special syntax to trick build system
+		})
+				
 		// shortly after onLoad, track the page (prevent UI blocking)
 		new dojox.analytics.Urchin({ 
 			acct: "UA-3572741-1", 
