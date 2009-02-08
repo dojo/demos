f48df173de179170edf3c0f9b4a8f7a6b9f61954
Peter E Higgins <dante@dojotoolkit.org>
2009-02-08 19:32:57 +0000

    lots of changes to castle demo as a result of accordionPane deprecation. lots of weirdness going on. IE plain fails if the scrollpane is in the AccordionContainer before startup() is called, and I've wasted too much time trying to track it down. of course it's !strict either
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@16591 560b804f-0ae3-0310-86f3-f6aa0a117693

--- castle/src.js
+++ castle/src.js
@@ -1,23 +1,21 @@
 dojo.require("dijit.layout.AccordionContainer");
 dojo.require("dojox.layout.ScrollPane");
 dojo.require("dojox.widget.FisheyeLite");
-dojo.require("dojox.analytics.Urchin");
-//dojo.require("dojox.layout.DragPane");
 dojo.require("dojo.NodeList-fx");
 dojo.require("dojo.fx");
 dojo.require("dojo.fx.easing");
+dojo.require("dojox.analytics.Urchin");
 
-var show = null;
-(function($){
-
-	show = function(id){
-		var contents = dojo.byId(id).innerHTML;
-		$("#content").style("opacity", 0).forEach(function(n){ n.innerHTML = contents; }).anim({ opacity:1 });
-	}
+;(function(){
 	
+	window.show = function(id){
+		var contents = dojo.byId(id).innerHTML;
+		dojo.query("#content").style("opacity", 0).forEach(function(n){ n.innerHTML = contents; }).anim({ opacity:1 });
+	}
+
 	var init = function(){
 			// turn li's in this page into fisheye items, presumtiously:  
-		 $("#hidden ul > li").forEach(function(n){
+		dojo.query("#hidden ul > li").forEach(function(n){
 			new dojox.widget.FisheyeLite({
 				properties:{
 				  fontSize:1.5
@@ -26,41 +24,31 @@ var show = null;
 				durationIn: 100,
 				easeOut: dojo.fx.easing.linear,
 				durationOut: 100
-			  }, n);
-		  });
-	
-		panes = [];
+			}, n);
+		});
+
+		var panes = [];
 		var paneType = dojo.getObject("dojox.layout.ScrollPane");
+  
 	
-		dojo.forEach(["day1s","day2s","day3s"], function(id){
-			var scroll = new paneType({
-				  style: "width:450px; height:170px"
-			}, id);
-			scroll.startup();
-			panes.push(scroll);			
-		});
-	
-		//accordion widget
-		accordion = new dijit.layout.AccordionContainer({}, "accordionPanel");
-		var content1 = new dijit.layout.AccordionPane({ id:'pane1', title: '25.07.2008', selected: true },'day1').placeAt(accordion);
-		var content2 = new dijit.layout.AccordionPane({ id:'pane2', title: '26.07.2008' }, 'day2').placeAt(accordion);
-		var content3 = new dijit.layout.AccordionPane({ id:'pane3', title: '27.07.2008' }, 'day3').placeAt(accordion);
-	
-		dojo.forEach([content1, content2, content3], function(pane,i){
-			// store a ref to the scrollpane contained within each accordionpane
-			dojo.mixin(pane, {
-				innerScroller: panes[i]
-			});
-		});
+	  	//accordion widget
+	  	accordion = new dijit.layout.AccordionContainer({}, "accordionPanel");
+
+	  	var content1 = new dijit.layout.ContentPane({ id:'pane1', title: '25.07.2008', selected: true },'day1').placeAt(accordion);
+	  	var content2 = new dijit.layout.ContentPane({ id:'pane2', title: '26.07.2008' }, 'day2').placeAt(accordion);
+	  	var content3 = new dijit.layout.ContentPane({ id:'pane3', title: '27.07.2008' }, 'day3').placeAt(accordion);
+		accordion.startup();
 
 		dojo.subscribe("accordionPanel-selectChild", function(child){
-			setTimeout(dojo.hitch(child.innerScroller,"layout"), 300);
+			// we do this because despite accordion passing correct sizes, scrollpane uses it's scrollheight/etc for sizing
+			setTimeout(dojo.hitch(child, "_layoutChildren"), 300);
 		});
 
-		accordion.startup();
-		content1.innerScroller.layout();
-	
-		$('.dijitAccordionText').style('opacity', 0.01);
+		dojo.forEach(["day1s","day2s","day3s"], function(id){
+			new paneType({ style: "width:450px;height:170px" }, id);
+		});
+
+		dojo.query('.dijitAccordionText').style('opacity', 0.01);
 
 		// demo usage tracking: 
 		new dojox.analytics.Urchin({ 
@@ -74,4 +62,4 @@ var show = null;
 
 	dojo.addOnLoad(init);
 
-})(dojo.query);
+})();
