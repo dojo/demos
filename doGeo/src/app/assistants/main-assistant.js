a099aef2ade47f604c587262db9385c42929083e
Shane O'Sullivan <shaneosullivan1@gmail.com>
2010-08-01 15:23:07 +0000

    Refs #11518 Add in google image search code
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@22654 560b804f-0ae3-0310-86f3-f6aa0a117693

--- doGeo/src/app/assistants/main-assistant.js
+++ doGeo/src/app/assistants/main-assistant.js
@@ -5,6 +5,8 @@ dojo.declare("MainAssistant", dojox.mobile.app.SceneAssistant, {
   
   setup: function(){
     console.log("In main assistant setup");
+	
+	this.handleAddressResults = dojo.hitch(this, this.handleAddressResults);
     
     // Instantiate widgets in the template HTML.
     this.controller.parse();
@@ -38,6 +40,57 @@ dojo.declare("MainAssistant", dojox.mobile.app.SceneAssistant, {
   activate: function(data){
     console.log("In main assistant activate");
     
+	//this.findAddressImages(["London", "Paris"], this.handleAddressResults);
+  },
+  
+  findAddressImages: function(addresses, callback){
+  	//http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20query.multi%20where%20queries%3D%22
+	//select%20*%20from%20json%20where%20url%3D%27http%3A%2F%2Fajax.googleapis.com%2Fajax%2Fservices%2Fsearch%2Fimages%3Fv%3D1.0%26q%3DLondon%26count%3D1%27
+	//%3B
+	//select%20*%20from%20json%20where%20url%3D%27http%3A%2F%2Fajax.googleapis.com%2Fajax%2Fservices%2Fsearch%2Fimages%3Fv%3D1.0%26q%3DParis%26count%3D1%27
+	//%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
+	
+    var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20query.multi%20where%20queries%3D%22";
+    var urlEnd = "%27%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys;"
+
+    url += dojo.map(addresses, function(address){
+		return "select%20*%20from%20json%20where%20url%3D%27http%3A%2F%2F" + 
+		"ajax.googleapis.com%2Fajax%2Fservices%2Fsearch%2Fimages%3Fv%3D1.0%26q%3D"
+		+ address
+		+ "%26count%3D1%27";
+	}).join("%3B") + "%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+	
+	
+	console.log("url = ", url);
+	
+    var deferred = dojo.io.script.get({
+		url: url,
+		jsonp: "callback"
+	});
+	deferred.addBoth(callback);
+  },
+  
+  handleAddressResults: function(data){
+  	console.log("handleAddressResults ", data);
+	
+	if(!data || !data.query || !data.query.results || !data.query.results.results){
+		// nothing returned
+		console.log("Wonky data?");
+		return null;
+	}
+	
+	var imageUrls = dojo.map(data.query.results.results, this.parseImageResults);
+	
+	console.log("Got image urls ", imageUrls);
+	dojo.publish("/images", [imageUrls]);
+	return imageUrls;
+  },
+  
+  parseImageResults: function(data){
+  	if(!data.json || !data.json.responseData || !data.json.responseData.results){
+		return null;
+	}
+	return data.json.responseData.results[0].tbUrl;
   },
   
   setService: function(name){
