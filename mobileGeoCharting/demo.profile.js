c1d255b02fce1e0ecc50c7de14db68e4b6621450
Chris Mitchell <ccmitchellusa@gmail.com>
2011-05-03 16:09:38 +0000

    fixes #12417 Added new mobile geo charting demo !strict
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@24610 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ mobileGeoCharting/demo.profile.js
@@ -0,0 +1,17 @@
+dependencies = {
+	layers: [
+		{
+			name: "../demos/mobileGeoCharting/src.js",
+			resourceName: "demos.mobileGeoCharting.src",
+			dependencies: [
+				"demos.mobileGeoCharting.src"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ],
+		[ "demos", "../demos" ]
+	]
+}
