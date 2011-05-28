b18471d92669271e1059a2d036f7ca31c998035c
Chris Mitchell <ccmitchellusa@gmail.com>
2011-05-28 04:42:13 +0000

    fixes #13097 updated demo READMEs and build profiles
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@25231 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ mobileFileBrowser/demo.profile.js
@@ -0,0 +1,17 @@
+dependencies = {
+	layers: [
+		{
+			name: "../demos/mobileFileBrowser/src.js",
+			resourceName: "demos.mobileFileBrowser.src",
+			dependencies: [
+				"demos.mobileFileBrowser.src"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ],
+		[ "demos", "../demos" ]
+	]
+}
