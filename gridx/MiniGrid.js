58d3f2872be8bb67a2e6ef456086a01bd53fcf61
Evan Huang <evanhuangwei@gmail.com>
2011-06-28 08:18:44 +0000

    Refs #11903 use syntax of require([], func()) instead of the dprecated dojo.require(...)
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@25646 560b804f-0ae3-0310-86f3-f6aa0a117693

--- gridx/MiniGrid.js
+++ gridx/MiniGrid.js
@@ -1,46 +1,48 @@
-dojo.require('demos.gridx.src.Grid');
-dojo.require('demos.gridx.src.modules.Header');
-dojo.require('demos.gridx.src.modules.Body');
-dojo.require('demos.gridx.src.modules.Layout');
-dojo.require('demos.gridx.src.modules.VScroller');
-dojo.require('demos.gridx.src.modules.HScroller');
-dojo.require('demos.gridx.src.modules.SingleSort');
-dojo.require('demos.gridx.src.core.model.SyncCache');
-dojo.require('demos.gridx.src.modules.ColumnResizer');
-
-dojo.require("dojo.data.ItemFileWriteStore");
-
-var layout = [
-	{ field: "id", name:"Index", width:'60px'},
-	{ field: "Genre", name:"Genre", width:'150px'},
-	{ field: "Artist", name:"Artist", width:'150px'},
-	{ field: "Year", name:"Year"},
-	{ field: "Name", name:"Name", width:'150px'},
-	{ field: "Length", name:"Length", width:'60px'},
-	{ field: "Track", name:"Track", width:'50px'},
-	{ field: "Composer", name:"Composer"},
-	{ field: "Download Date", name:"Download Date", width:'100px'}
-];
-
-var mr = demos.gridx.src.modules;
-
-var modules = [
-	mr.Header,
-	mr.Body,
-	mr.Layout,
-	mr.HScroller,
-	mr.VScroller,
-	mr.SingleSort,
-	mr.ColumnResizer
-];
-
-dojo.ready(function(){
-	var grid = new demos.gridx.src.Grid({
-		id: 'grid',
-		store: getStore(200),
-		structure: layout,
-		modules: modules
-	});
-	grid.placeAt('gridDiv');
-	grid.startup();
+require([
+	'demos/gridx/src/Grid',
+	'demos/gridx/src/modules/Header',
+	'demos/gridx/src/modules/Body',
+	'demos/gridx/src/modules/Layout',
+	'demos/gridx/src/modules/VScroller',
+	'demos/gridx/src/modules/HScroller',
+	'demos/gridx/src/modules/SingleSort',
+	'demos/gridx/src/core/model/SyncCache',
+	'demos/gridx/src/modules/ColumnResizer',
+	"dojo/data/ItemFileWriteStore",
+	"dijit/_base"
+], function(){
+	var layout = [
+		{ field: "id", name:"Index", width:'60px'},
+		{ field: "Genre", name:"Genre", width:'150px'},
+		{ field: "Artist", name:"Artist", width:'150px'},
+		{ field: "Year", name:"Year"},
+		{ field: "Name", name:"Name", width:'150px'},
+		{ field: "Length", name:"Length", width:'60px'},
+		{ field: "Track", name:"Track", width:'50px'},
+		{ field: "Composer", name:"Composer"},
+		{ field: "Download Date", name:"Download Date", width:'100px'}
+	];
+	
+	var mr = demos.gridx.src.modules;
+	
+	var modules = [
+		mr.Header,
+		mr.Body,
+		mr.Layout,
+		mr.HScroller,
+		mr.VScroller,
+		mr.SingleSort,
+		mr.ColumnResizer
+	];
+	
+	dojo.ready(function(){
+		var grid = new demos.gridx.src.Grid({
+			id: 'grid',
+			store: getStore(200),
+			structure: layout,
+			modules: modules
+		});
+		grid.placeAt('gridDiv');
+		grid.startup();
+	});	
 });
--- gridx/MobileGrid.js
+++ gridx/MobileGrid.js
@@ -1,54 +1,56 @@
-dojo.require("demos.gridx.src.Grid");
-dojo.require("demos.gridx.src.modules.Header");
-dojo.require("demos.gridx.src.modules.Body");
-dojo.require("demos.gridx.src.modules.Layout");
-dojo.require("demos.gridx.src.modules.VScroller");
-dojo.require("demos.gridx.src.modules.HScroller");
-dojo.require("demos.gridx.src.modules.SingleSort");
-dojo.require("demos.gridx.src.modules.TouchScroller");
-dojo.require("demos.gridx.src.modules.Rotater");
-dojo.require("demos.gridx.src.core.model.SyncCache");
+require([
+	"demos/gridx/src/Grid",
+	"demos/gridx/src/modules/Header",
+	"demos/gridx/src/modules/Body",
+	"demos/gridx/src/modules/Layout",
+	"demos/gridx/src/modules/VScroller",
+	"demos/gridx/src/modules/HScroller",
+	"demos/gridx/src/modules/SingleSort",
+	"demos/gridx/src/modules/TouchScroller",
+	"demos/gridx/src/modules/Rotater",
+	"demos/gridx/src/core/model/SyncCache",
+	"dojo/data/ItemFileWriteStore",
+	"dojox/mobile/parser",
+	"dojox/mobile/View",
+	"dojox/mobile/deviceTheme",
+	"dijit/_base"
+], function(){
+	dojox.mobile.themeFiles = ["base", ["demos.gridx.src", "Gridx"]];
 
-dojo.require("dojo.data.ItemFileWriteStore");
-
-dojo.require("dojox.mobile.parser");
-dojo.require("dojox.mobile.View");
-dojox.mobile.themeFiles = ["base", ["demos.gridx.src", "Gridx"]];
-dojo.require("dojox.mobile.deviceTheme");
-
-var structure = [
-	{field: "id", name:"Index", width: "50px"},
-	{field: "Name", name:"Name", width: "135px"},
-	{field: "Artist", name:"Artist", width: "135px"}
-];
-var landscapeStructure = [
-	{field: "id", name:"Index", width: "50px"},
-	{field: "Name", name:"Name", width: "110px"},
-	{field: "Artist", name:"Artist", width: "110px"},
-	{field: "Length", name:"Length", width: "50px"},
-	{field: "Album", name:"Album", width: "110px"},
-	{field: "Year", name:"Year", width: "50px"}
-];
-
-var mr = demos.gridx.src.modules;
-
-var modules = [
-	mr.Header,
-	mr.Body,
-	mr.Layout,
-	mr.SingleSort,
-	mr.TouchScroller,
-	mr.Rotater
-];
-
-dojo.ready(function(){
-	var grid = new demos.gridx.src.Grid({
-		id: "grid",
-		store: getStore(100),
-		structure: structure,
-		landscapeStructure: landscapeStructure,
-		modules: modules
+	var structure = [
+		{field: "id", name:"Index", width: "50px"},
+		{field: "Name", name:"Name", width: "135px"},
+		{field: "Artist", name:"Artist", width: "135px"}
+	];
+	var landscapeStructure = [
+		{field: "id", name:"Index", width: "50px"},
+		{field: "Name", name:"Name", width: "110px"},
+		{field: "Artist", name:"Artist", width: "110px"},
+		{field: "Length", name:"Length", width: "50px"},
+		{field: "Album", name:"Album", width: "110px"},
+		{field: "Year", name:"Year", width: "50px"}
+	];
+	
+	var mr = demos.gridx.src.modules;
+	
+	var modules = [
+		mr.Header,
+		mr.Body,
+		mr.Layout,
+		mr.SingleSort,
+		mr.TouchScroller,
+		mr.Rotater
+	];
+	
+	dojo.ready(function(){
+		var grid = new demos.gridx.src.Grid({
+			id: "grid",
+			store: getStore(100),
+			structure: structure,
+			landscapeStructure: landscapeStructure,
+			modules: modules
+		});
+		grid.placeAt("gridDiv");
+		grid.startup();
 	});
-	grid.placeAt("gridDiv");
-	grid.startup();
 });
