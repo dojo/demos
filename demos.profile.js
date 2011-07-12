6ad1e9f8902e08732779249027fbc7e418f186c1
Rawld Gill <rgill@altoviso.com>
2011-07-12 12:03:52 +0000

    moved legacy loader API out of loader into separate module; completed dojo/require and associated dojo/loadInit plugins; removed remaining bdLoad cruft; fixed/added tests; other cleanup (see #13255); fixes #13255; !strict
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@25768 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ demos.profile.js
@@ -0,0 +1,38 @@
+(function(){
+	var testResourceRe = /\/tests\//,
+		copyOnly = function(mid){
+			var list = {
+				"demos/demos.profile":1,
+				"demos/package.json":1,
+				"demos/README":1
+			};
+			return (mid in list) || /^demos\/resources\//.test(mid);
+		},
+		usesDojoProvideEtAl = function(mid){
+			return /^demos\/(babelChat|beer|castle|cropper|css3|demail|doGeo|faces|fisheye|flashCards|fonts|form|i18n|mail|mojo|nihao|skew|survey|uploader|video)\//.test(mid);
+		};
+
+	return {
+		resourceTags:{
+			test: function(filename, mid){
+				return testResourceRe.test(mid);
+			},
+
+			copyOnly: function(filename, mid){
+				return copyOnly(mid);
+			},
+
+			amd: function(filename, mid){
+				return !testResourceRe.test(mid) && !copyOnly(mid) && !usesDojoProvideEtAl(mid) && /\.js$/.test(filename);
+			},
+
+			miniExclude: function(filename, mid){
+				return 0;
+			}
+		},
+
+		trees:[
+			[".", ".", /(\/\.)|(~$)/]
+		]
+	};
+})()
