8d98992ac43252991405dd54b51d8c9294308fb5
Patrick Ruzand <pruzand@gmail.com>
2011-12-01 17:56:04 +0000

    Butterfly demo. refs #14360
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@27175 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ butterfly/demo.profile.js
@@ -0,0 +1,17 @@
+dependencies = {
+	layers: [
+		{
+			name: "../demos/butterfly/src.js",
+			resourceName: "demos.butterfly.src",
+			dependencies: [
+				"demos.butterfly.src"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ],
+		[ "demos", "../demos" ]
+	]
+}
