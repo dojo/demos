b0d03d0c04b5de1f0fbe96ff95edf97c29ea26b5
Christophe Jolif <cjolif@gmail.com>
2011-12-01 15:26:47 +0000

    refs #14360. Charting type demo.
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@27171 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ chartTypes/demo.profile.js
@@ -0,0 +1,16 @@
+dependencies = {
+	layers: [
+		{
+			name: "../demos/chartTypes/src.js",
+			resourceName: "demos.chartTypes.src",
+			dependencies: [
+				"demos.chartTypes.src"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dojox", "../dojox" ],
+		[ "demos", "../demos" ]
+	]
+}
