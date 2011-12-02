81f078647c9ae59a7d59b9ed6bf6f3dcb9d5bf40
Christophe Jolif <cjolif@gmail.com>
2011-12-02 12:02:30 +0000

    refs #14360. dynamic chart.
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@27212 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ dynamicChart/demo.profile.js
@@ -0,0 +1,17 @@
+dependencies = {
+	layers: [
+		{
+			name: "../demos/dynamicChart/src.js",
+			resourceName: "demos.dynamicChart.src",
+			dependencies: [
+				"demos.dynamicChart.src"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ],
+		[ "demos", "../demos" ]
+	]
+}
