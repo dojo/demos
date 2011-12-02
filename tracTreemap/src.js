f27747b6b5f01f175a03a474a3a0ce0942440366
Christophe Jolif <cjolif@gmail.com>
2011-12-02 09:09:48 +0000

    refs #14319. get xdomain data working thanks to neonstalwart using yql.
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@27192 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tracTreemap/src.js
+++ tracTreemap/src.js
@@ -71,79 +71,112 @@ var sizeBySeverityFunc = function(item){
 require(["dojo/ready", "dojo/dom", "dojo/_base/Color", "dojo/_base/declare", "dojo/parser",
 	"dijit/registry", "dijit/Tooltip", "dojo/dom-style", "dojo/dom-attr", "dojo/dom-construct",
 	"dijit/layout/BorderContainer", "dijit/layout/ContentPane", "dojox/treemap/TreeMap",
-	"dojox/data/CsvStore", "dojo/store/DataStore",
 	"dijit/form/RadioButton", "dojox/treemap/Keyboard",
-	"dojox/treemap/DrillDownUp"],
-	function(ready, dom, Color, declare, parser, registry, Tooltip, domStyle, domAttr, domConstruct, BorderContainer, ContentPane, TreeMap, CsvStore, DataStore, RadioButton, Keyboard, DrillDownUp){
+	"dojox/treemap/DrillDownUp", "dojo/store/Memory", "dojo/store/Observable",
+	"dojo/io/script", "dojo/_base/Deferred", "dojo/_base/array"],
+	function(ready, dom, Color, declare, parser, registry, Tooltip, 
+			domStyle, domAttr, domConstruct, 
+			BorderContainer, ContentPane, TreeMap, RadioButton, Keyboard, DrillDownUp,
+			Memory, Observable, script, Deferred, array){
 
-		// xhrPlugins.addCrossSiteXhr("http://trac.dojotoolkit.org");
-		// var store = new DataStore({ store: new CsvStore({url: "http://trac.dojotoolkit.org/report/132?asc=1&format=csv"}) });
-		// XSite from archive to trac does not work, use local copy
-		var store = new DataStore({ store: new CsvStore({url: "report_132.csv"}) });
+	//var store = new DataStore({ store: new CsvStore({url: "http://trac.dojotoolkit.org/report/132?asc=1&format=csv"}) });
+	//var store = new DataStore({ store: new CsvStore({url: "report_132.csv"}) });
 
-		ready(function(){
-			MyTreeMap=declare([TreeMap, Keyboard, DrillDownUp], {
-				createRenderer: function(item, level, kind){
-					if(kind == "leaf"){
-						var div=domConstruct.create("a");
-						domAttr.set(div, "href", "http://bugs.dojotoolkit.org/ticket/" + item.ticket);
-						domStyle.set(div, "overflow", "hidden");
-						domStyle.set(div, "position", "absolute");
-						return div;
-					} else{
-						return this.inherited(arguments);
-					}
-				}
-			});
-			parser.parse();
-			var treeMap=registry.byId("treeMap");
-			treeMap.set("colorFunc", colorByPriorityFunc);
-			treeMap.set("areaFunc", sizeBySeverityFunc);
-			treeMap.set("groupAttrs", ["component"]);
+	var query = 'select * from csv where url="http://trac.dojotoolkit.org/report/132?asc=1&format=csv"';
+	var request = script.get({
+		url: "http://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(query) + "&format=json",
+		jsonp: "callback"
+	});
+	var store;
+	
+	Deferred.when(request, function(response){
+		var results = response.query.results.row;
+		var header = results[0];
+		var rows = results.slice(1);
+		var data = array.map(rows, function (row) {
+			var ticket = {};
+			for(var prop in row){
+				ticket[header[prop]] = row[prop];
+			}
+			return ticket;
+		});			
+		store = Observable(new Memory({data: data}));
+		// depending on when we arrive here treemap
+		// might already been there...
+		// reset data:
+		var treeMap = registry.byId("treeMap");		
+		if(treeMap){
 			treeMap.set("store", store);
-			treeMap.onItemRollOver=function(evt){
-				if(evt.item.summary){
-					Tooltip.show(evt.item.summary, evt.renderer);
+		}
+	}, function(err){
+		console.log("could not reach data source");
+	});		
+		
+	ready(function(){
+		MyTreeMap = declare([TreeMap, Keyboard, DrillDownUp], {
+			createRenderer: function(item, level, kind){
+				if(kind == "leaf"){
+					var div = domConstruct.create("a");
+					domAttr.set(div, "href", "http://bugs.dojotoolkit.org/ticket/" + item.ticket);
+					domStyle.set(div, "overflow", "hidden");
+					domStyle.set(div, "position", "absolute");
+					return div;
+				}else{
+					return this.inherited(arguments);
 				}
-			};
-			treeMap.onItemRollOut=function(evt){
-				Tooltip.hide(evt.renderer);
-			};
+			}
 		});
-
-		groupByChanged=function(value){
-			var groupBy=null;
-			if(dom.byId("g2").checked){
-				groupBy=["owner"];
-			} else if(dom.byId("g3").checked){
-				groupBy=["component"];
-			} else if(dom.byId("g4").checked){
-				groupBy=["milestone"];
-			}
-			var treeMap=registry.byId("treeMap");
-			if(groupBy != null){
-				treeMap.set("groupAttrs", groupBy);
-			} else{
-				treeMap.set("groupAttrs", null);
+		parser.parse();
+		var treeMap = registry.byId("treeMap");
+		treeMap.set("colorFunc", colorByPriorityFunc);
+		treeMap.set("areaFunc", sizeBySeverityFunc);
+		treeMap.set("groupAttrs", ["component"]);
+		if(store){
+			treeMap.set("store", store);
+		}
+		treeMap.onItemRollOver=function(evt){
+			if(evt.item.summary){
+				Tooltip.show(evt.item.summary, evt.renderer);
 			}
 		};
-
-		sizeByChanged=function(value){
-			var treeMap=registry.byId("treeMap");
-			if(dom.byId("s2").checked){
-				treeMap.set("areaFunc", sizeByPriorityFunc);
-			} else if(dom.byId("s3").checked){
-				treeMap.set("areaFunc", sizeBySeverityFunc);
-			}
-		};
-
-		colorByChanged=function(value){
-			var colorBy=null;
-			var treeMap=registry.byId("treeMap");
-			if(dom.byId("c2").checked){
-				treeMap.set("colorFunc", colorByPriorityFunc);
-			} else if(dom.byId("c3").checked){
-				treeMap.set("colorFunc", colorBySeverityFunc);
-			}
+		treeMap.onItemRollOut=function(evt){
+			Tooltip.hide(evt.renderer);
 		};
 	});
+
+	groupByChanged=function(value){
+		var groupBy = null;
+		if(dom.byId("g2").checked){
+			groupBy = ["owner"];
+		}else if(dom.byId("g3").checked){
+			groupBy = ["component"];
+		}else if(dom.byId("g4").checked){
+			groupBy = ["milestone"];
+		}
+		var treeMap = registry.byId("treeMap");
+		if(groupBy != null){
+			treeMap.set("groupAttrs", groupBy);
+		}else{
+			treeMap.set("groupAttrs", null);
+		}
+	};
+
+	sizeByChanged = function(value){
+		var treeMap = registry.byId("treeMap");
+		if(dom.byId("s2").checked){
+			treeMap.set("areaFunc", sizeByPriorityFunc);
+		}else if(dom.byId("s3").checked){
+			treeMap.set("areaFunc", sizeBySeverityFunc);
+		}
+	};
+
+	colorByChanged = function(value){
+		var colorBy = null;
+		var treeMap = registry.byId("treeMap");
+		if(dom.byId("c2").checked){
+			treeMap.set("colorFunc", colorByPriorityFunc);
+		}else if(dom.byId("c3").checked){
+			treeMap.set("colorFunc", colorBySeverityFunc);
+		}
+	};
+});
