c6914d2a1007f282f0fb8c6de03b0987b14d4986
Christophe Jolif <cjolif@gmail.com>
2011-12-02 09:50:36 +0000

    refs #14360. Day / night map demo. !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@27202 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ sunMap/demo.profile.js
@@ -0,0 +1,17 @@
+dependencies = {
+	layers: [
+		{
+			name: "../demos/mobileCharting/src.js",
+			resourceName: "demos.mobileCharting.src",
+			dependencies: [
+				"demos.mobileCharting.src"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ],
+		[ "demos", "../demos" ]
+	]
+}
