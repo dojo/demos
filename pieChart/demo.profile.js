5111782eefff9951d155caf642904eb59f452633
Christophe Jolif <cjolif@gmail.com>
2011-12-02 13:01:01 +0000

    refs #14360. pie chart.
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@27213 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ pieChart/demo.profile.js
@@ -0,0 +1,17 @@
+dependencies = {
+	layers: [
+		{
+			name: "../demos/pieChart/src.js",
+			resourceName: "demos.pieChart.src",
+			dependencies: [
+				"demos.pieChart.src"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ],
+		[ "demos", "../demos" ]
+	]
+}
