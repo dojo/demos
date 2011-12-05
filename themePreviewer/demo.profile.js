d0e18b3a595694566532a8e7815ad5b8833ee014
Douglas Hays <doughays@dojotoolkit.org>
2011-12-05 17:40:42 +0000

    Refs #14360.  Create themePreviewer demo from dijit/themes/themeTester.html
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@27271 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ themePreviewer/demo.profile.js
@@ -0,0 +1,17 @@
+dependencies = {
+	layers: [
+		{
+			name: "../demos/themePreviewer/src.js",
+			resourceName: "demos.themePreviewer.src",
+			dependencies: [
+				"demos.themePreviewer.src"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ],
+		[ "demos", "../demos" ]
+	]
+}
