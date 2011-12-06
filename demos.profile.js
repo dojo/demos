49cf377ea57116ffdc45ce47a305f1757b9681df
Rawld Gill <rgill@altoviso.com>
2011-12-06 08:23:08 +0000

    backport [27292] and [27291]; fixes #1440; fixes #14161
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.7/demos@27300 560b804f-0ae3-0310-86f3-f6aa0a117693

--- demos.profile.js
+++ demos.profile.js
@@ -1,36 +1,40 @@
-var testResourceRe = /\/tests\//,
-	copyOnly = function(mid){
-		var list = {
-			"demos/demos.profile":1,
-			"demos/package.json":1,
-			"demos/README":1
+var profile = (function(){
+	var testResourceRe = /\/tests\//,
+
+		copyOnly = function(filename, mid){
+			var list = {
+				"demos/demos.profile":1,
+				"demos/package.json":1,
+				"demos/README":1
+			};
+			return (mid in list) || (/^demos\/resources\//.test(mid) && !/\.css$/.test(filename)) || /(png|jpg|jpeg|gif|tiff)$/.test(filename);
+		},
+
+		usesDojoProvideEtAl = function(mid){
+			return /^demos\/(babelChat|beer|castle|cropper|css3|demail|doGeo|faces|fisheye|flashCards|fonts|form|i18n|mail|mojo|nihao|skew|survey|uploader|video)\//.test(mid);
 		};
-		return (mid in list) || (/^demos\/resources\//.test(mid) && !/\.css$/.test(mid));
-	},
-	usesDojoProvideEtAl = function(mid){
-		return /^demos\/(babelChat|beer|castle|cropper|css3|demail|doGeo|faces|fisheye|flashCards|fonts|form|i18n|mail|mojo|nihao|skew|survey|uploader|video)\//.test(mid);
+
+	return {
+		resourceTags:{
+			test: function(filename, mid){
+				return testResourceRe.test(mid);
+			},
+
+			copyOnly: function(filename, mid){
+				return copyOnly(filename, mid);
+			},
+
+			amd: function(filename, mid){
+				return !testResourceRe.test(mid) && !copyOnly(filename, mid) && !usesDojoProvideEtAl(mid) && /\.js$/.test(filename);
+			},
+
+			miniExclude: function(filename, mid){
+				return 0;
+			}
+		},
+
+		trees:[
+			[".", ".", /(\/\.)|(~$)/]
+		]
 	};
-
-var profile = {
-	resourceTags:{
-		test: function(filename, mid){
-			return testResourceRe.test(mid);
-		},
-
-		copyOnly: function(filename, mid){
-			return copyOnly(mid);
-		},
-
-		amd: function(filename, mid){
-			return !testResourceRe.test(mid) && !copyOnly(mid) && !usesDojoProvideEtAl(mid) && /\.js$/.test(filename);
-		},
-
-		miniExclude: function(filename, mid){
-			return 0;
-		}
-	},
-
-	trees:[
-		[".", ".", /(\/\.)|(~$)/]
-	]
-};
+})();
