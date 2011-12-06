5588b82a74751bf54e248b8f06d3a433079fde5b
Evan Huang <evanhuangwei@gmail.com>
2011-12-06 15:43:00 +0000

    Refs #14360 !strict Add grid all-in-one demo (including icon)
    git-svn-id: http://svn.dojotoolkit.org/src/demos/trunk@27304 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ grid/demo.profile.js
@@ -0,0 +1,17 @@
+dependencies = {
+	layers: [
+		{
+			name: "../demos/grid/src.js",
+			resourceName: "demos.grid.src",
+			dependencies: [
+				"demos.grid.src"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ],
+		[ "demos", "../demos" ]
+	]
+}
